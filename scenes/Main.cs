d48d7bbe400c6a09e07592b2d0e7c5cf0b95af78
Anton Mulder <mulderanton89@gmail.com>
2025-09-15 19:32:43 +0200

    5. Instantiating New Scenes

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -4,20 +4,40 @@ namespace Game;
 
 public partial class Main : Node2D
 {
-    private Sprite2D sprite;
+    private Sprite2D _sprite;
+    private PackedScene _buildingScene;
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        sprite = GetNode<Sprite2D>("Cursor");
+        _buildingScene = GD.Load<PackedScene>("res://scenes/building/Building.tscn");
+        _sprite = GetNode<Sprite2D>("Cursor");
     }
 
+    public override void _UnhandledInput(InputEvent evt)
+    {
+        if (evt.IsActionPressed("left_click")) PlaceBuildingAtMousePosition();
+    }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
+    {
+        var gridPosition = GetMouseGridCellPosition();
+        _sprite.GlobalPosition = gridPosition * 64;
+    }
+
+    private Vector2 GetMouseGridCellPosition()
     {
         var mousePosition = GetGlobalMousePosition();
         var gridPosition = (mousePosition / 64).Floor();
-        sprite.GlobalPosition = gridPosition * 64;
+
+        return gridPosition;
+    }
+
+    private void PlaceBuildingAtMousePosition()
+    {
+        var building = _buildingScene.Instantiate<Node2D>();
+        AddChild(building);
+
+        var gridPosition = GetMouseGridCellPosition();
+        building.GlobalPosition = gridPosition * 64;
     }
 }
